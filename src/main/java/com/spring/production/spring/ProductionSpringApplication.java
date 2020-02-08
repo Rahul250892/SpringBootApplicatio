226eed047a9d010e4e542a1133819e491928e23b
rahul250892 <rahulkumar250892@outlook.com>
2020-02-08 11:47:32 +0530

    first commit

---
+++ src/main/java/com/spring/production/spring/ProductionSpringApplication.java
@@ -0,0 +1,13 @@
+package com.spring.production.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductionSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductionSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spring/production/spring/ProductionSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.production.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductionSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
